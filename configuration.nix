bb73b42945683fe2a844831983f6d78d85257afb
Zhu Zihao <all_but_last@163.com>
2020-06-11 00:24:29 +0800

    Initial commit.

---
+++ configuration.nix
@@ -0,0 +1,137 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+let unstablePkg = import <nixpkgs-unstable> {};
+in
+{
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./services/v2ray-ipv4-transproxy.nix
+    ];
+
+  nix.binaryCaches = [
+    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "/dev/sda";
+    useOSProber = true;
+  };
+
+  fonts = {
+    fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      emacs-all-the-icons-fonts
+    ] ++ [ unstablePkg.sarasa-gothic ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "Sarsa Mono SC" "DejaVu Sans Mono" ];
+        emoji = [ "Noto Color Emoji" ];
+        sansSerif = [ "Noto Sans CJK SC" ];
+        serif = [ "Noto Serif CJK SC" ];
+      };
+    };
+  };
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp4s0.useDHCP = true;
+  networking.interfaces.wlp0s20u9.useDHCP = true;
+
+  services.v2ray-ipv4-transproxy = {
+    enable = true;
+    redirPort = 7892;
+    configPath = ./secrets/v2ray/config.json;
+  };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "zh_CN.UTF-8";
+  i18n.inputMethod.enabled = "fcitx";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Asia/Shanghai";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    binutils clang llvm git
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  #   pinentryFlavor = "gnome3";
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+
+  programs.gnupg.agent.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.citreu = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+   };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.03"; # Did you read the comment?
+
+}
---
+++ services/v2ray-ipv4-transproxy.nix
@@ -0,0 +1,93 @@
+{ pkgs, config, lib, ... }:
+
+with lib;
+
+let
+  inherit (pkgs) gnugrep iptables v2ray;
+  inherit (lib) optionalString mkIf;
+  cfg = config.services.v2ray-ipv4-transproxy;
+  inherit (cfg) v2rayUserName;
+  inherit (cfg) configPath;
+  redirProxyPortStr = toString cfg.redirPort;
+
+  tag = "V2RAY_SPEC";
+
+  opts = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      # FIXME: Description
+    };
+
+    v2rayUserName = mkOption {
+      type = types.str;
+      default = "v2ray";
+      description =
+        "The user who would run the v2ray proxy systemd service. will be created automatically.";
+    };
+
+    redirPort = mkOption {
+      type = types.port;
+      default = 1081;
+      description =
+        "Proxy local redir server (<literal>ss-redir</literal>) listen port";
+    };
+
+    configPath = mkOption {
+      type = types.path;
+      description = "The v2ray configuration file";
+    };
+  };
+
+in {
+  options = {
+    services.v2ray-ipv4-transproxy = opts;
+  };
+
+  config = mkIf (cfg.enable) {
+
+    users.users.${v2rayUserName} = {
+      description = "v2ray deamon user";
+      isSystemUser = true;
+    };
+
+    boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+
+    systemd.services.v2ray-ipv4-transproxy = let
+      ipt = "${iptables}/bin/iptables";
+      preStartScript = pkgs.writeShellScript "v2ray-prestart" ''
+        ${ipt} -t nat -F ${tag}
+        ${ipt} -t nat -N ${tag}
+        ${ipt} -t nat -A ${tag} -j RETURN -m owner --uid-owner ${v2rayUserName}
+        ${ipt} -t nat -A ${tag} -p tcp -j REDIRECT --to-ports ${redirProxyPortStr}
+        ${ipt} -t nat -A OUTPUT -p tcp -j ${tag}
+        '';
+
+      postStopScript = pkgs.writeShellScript "v2ray-poststop" ''
+        ${iptables}/bin/iptables-save -c \
+        | ${gnugrep}/bin/grep -v ${tag} \
+        | ${iptables}/bin/iptables-restore -c
+        '';
+    in {
+      description = "V2ray transparent proxy service";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      script =
+        "exec ${v2ray}/bin/v2ray -config ${toString configPath}";
+
+      # Don't start if the config file doesn't exist.
+      unitConfig = { ConditionPathExists = configPath; };
+      serviceConfig = {
+        ExecStartPre =
+          "+${preStartScript}"; # Use prefix `+` to run iptables as root/
+        ExecStopPost = "+${postStopScript}";
+        # CAP_NET_BIND_SERVICE: Bind arbitary ports by unprivileged user.
+        # CAP_NET_ADMIN: Listen on UDP.
+        AmbientCapabilities =
+          "CAP_NET_BIND_SERVICE CAP_NET_ADMIN"; # We want additional capabilities upon a unprivileged user.
+        User = v2rayUserName;
+        Restart = "on-failure";
+      };
+    };
+  };
+}
